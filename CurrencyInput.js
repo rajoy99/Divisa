65b0e3f32c4503c2fe21cf989fea897276e43834
rajoy99 <rafat.joy99@gmail.com>
2022-02-21 00:04:18 +0600

    many

---
+++ CurrencyInput.js
@@ -0,0 +1,31 @@
+import { Select } from "@mui/material"
+import React  from "react"
+import './CurrencyInput.css'
+import {View} from 'react-native`'
+
+
+
+const CurrencyInput = (props) => {
+
+    return (
+      <View className="group">
+        <input
+          type="number"
+          value={props.amount}
+          onChange={(ev) => props.onAmountChange(ev.target.value)}
+        />
+
+        <select
+          value={props.currency}
+          onChange={(ev) => props.onCurrencyChange(ev.target.value)}
+        >
+          {props.currencies.map((currency) => (
+            <option value={currency}> {currency} </option>
+          ))}
+        </select>
+      </View>
+    );
+
+}
+
+export default CurrencyInput
