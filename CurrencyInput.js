47e67974f8eb62d54906b694fdd4e552a279da45
rajoy99 <rafat.joy99@gmail.com>
2022-02-28 01:17:18 +0600

    picker

--- CurrencyInput.js
+++ CurrencyInput.js
@@ -1,29 +1,34 @@
-import React  from "react"
-import {View} from 'react-native'
-
-
+import React from "react";
+import { TextInput, View } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 
 const CurrencyInput = (props) => {
+  return (
+    // <View className="group">
+    //   <TextInput
+    //     type="number"
+    //     value={props.amount}
+    //     onChange={(ev) => props.onAmountChange(ev.target.value)}
+    //   />
 
-    return (
-      <View className="group">
-        <input
-          type="number"
-          value={props.amount}
-          onChange={(ev) => props.onAmountChange(ev.target.value)}
-        />
+    //   <Select
+    //     value={props.currency}
+    //     onChange={(ev) => props.onCurrencyChange(ev.target.value)}
+    //   >
+    //     {props.currencies.map((currency) => (
+    //       <option value={currency}> {currency} </option>
+    //     ))}
+    //   </Select>
+    // </View>
 
-        <select
-          value={props.currency}
-          onChange={(ev) => props.onCurrencyChange(ev.target.value)}
-        >
-          {props.currencies.map((currency) => (
-            <option value={currency}> {currency} </option>
-          ))}
-        </select>
-      </View>
-    );
+    <Picker
+      selectedValue={props.currency}
+      onValueChange={(ev) => props.onCurrencyChange(ev.target.value)}
+    >
+      {/* <Picker.Item label="Java" value="java" />
+      <Picker.Item label="JavaScript" value="js" /> */}
+    </Picker>
+  );
+};
 
-}
-
-export default CurrencyInput
+export default CurrencyInput;
