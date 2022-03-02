41db32f2263bdc835b9ab7e3b5626a9e56f6ade6
rajoy99 <rafat.joy99@gmail.com>
2022-03-02 20:51:10 +0600

    some changes

--- App.js
+++ App.js
@@ -1,9 +1,35 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { TextInput, StyleSheet, Text, View } from "react-native";
 import { useState, useEffect } from "react";
-import CurrencyInput from "./CurrencyInput";
+// import CurrencyInput from "./CurrencyInput";
 import axios from "axios";
 import { Picker } from "@react-native-picker/picker";
+
+
+const CurrencyInput = (props) => {
+  return (
+    <View>
+      <TextInput
+        placeholder="Enter Amount to Convert"
+        onChange={(ev) => props.onAmountChange(ev.target.value)}
+        value={props.amount}
+        keyboardType="number-pad"
+        maxLength={12}
+      />
+
+      <Picker
+        selectedValue={props.currency}
+        onChange={(ev) => props.onCurrencyChange(ev.target.value)}
+      >
+        {props.currencies.map((currency) => (
+          <Picker.Item label={currency} value={currency} />
+        ))}
+      </Picker>
+    </View>
+  );
+};
+
+
 
 const App = () => {
   const [amount1, setamount1] = useState(1);
--- CurrencyInput.js
+++ CurrencyInput.js
@@ -4,26 +4,10 @@ import { Picker } from "@react-native-picker/picker";
 
 const CurrencyInput = (props) => {
   return (
-    // <View className="group">
-    //   <TextInput
-    //     type="number"
-    //     value={props.amount}
-    //     onChange={(ev) => props.onAmountChange(ev.target.value)}
-    //   />
-
-    //   <Select
-    //     value={props.currency}
-    //     onChange={(ev) => props.onCurrencyChange(ev.target.value)}
-    //   >
-    //     {props.currencies.map((currency) => (
-    //       <option value={currency}> {currency} </option>
-    //     ))}
-    //   </Select>
-    // </View>
     <View>
       <TextInput
         placeholder="Enter Amount to Convert"
-        onChangeText={(ev) => props.onAmountChange(ev.target.value)}
+        onChange={(ev) => props.onAmountChange(ev.target.value)}
         value={props.amount}
         keyboardType="number-pad"
         maxLength={12}
@@ -31,7 +15,7 @@ const CurrencyInput = (props) => {
 
       <Picker
         selectedValue={props.currency}
-        onValueChange={(ev) => props.onCurrencyChange(ev.target.value)}
+        onChange={(ev) => props.onCurrencyChange(ev.target.value)}
       >
         {props.currencies.map((currency) => (
           <Picker.Item label={currency} value={currency} />
